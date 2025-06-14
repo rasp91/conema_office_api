0288fae1fe8851f0491d0dbe400c628cd08177c4
Radek Springer <springer.radek@gmail.com>
2025-06-14 23:59:32 +0200

    Add form management endpoints and schemas for CRUD operations

---
+++ src/v1/forms/router.py
@@ -0,0 +1,117 @@
+import base64
+import re
+import os
+from pathlib import Path
+from uuid import uuid4
+
+from sqlalchemy.sql import func
+from sqlalchemy.orm import joinedload, Session
+from sqlalchemy import select, and_
+from fastapi import status, HTTPException, APIRouter, Depends
+
+from src.v1.forms.schemas import FormResponseModel, FormCreateModel, UserModel, FormModel
+from src.database.models import Form
+from src.auth.schemas import AuthUser
+from src.database import get_db
+from src.logger import logger
+from src.config import config
+from src.auth import get_admin_user
+
+router = APIRouter()
+
+
+@router.get(
+    "/get-form",
+    status_code=status.HTTP_200_OK,
+    name="Get Form",
+    response_model=FormModel,
+)
+def get_form(locate: str, db: Session = Depends(get_db)) -> None:
+    try:
+        # Get all forms from the database
+        form_data = db.execute(select(Form).where(Form.name == locate).options(joinedload(Form.updater))).scalar_one_or_none()
+        if not form_data:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Form with name '{locate}' not found.")
+        # Return the form data
+        return form_data
+    except Exception as e:
+        logger.exception(e)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"There is a problem with fetching form data.")
+
+
+@router.get(
+    "/get-forms",
+    status_code=status.HTTP_200_OK,
+    name="Get Forms",
+    dependencies=[Depends(get_admin_user)],
+    response_model=list[FormModel],
+)
+def get_forms(db: Session = Depends(get_db)) -> None:
+    try:
+        # Get all forms from the database
+        forms_data = db.execute(select(Form).options(joinedload(Form.updater))).scalars().all()
+        # Use Pydantic to convert ORM objects to dicts
+        return forms_data
+    except Exception as e:
+        logger.exception(e)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"There is a problem with fetching forms data.")
+
+
+@router.post(
+    "/create",
+    status_code=status.HTTP_200_OK,
+    name="Create Form",
+    dependencies=[Depends(get_admin_user)],
+    response_model=FormResponseModel,
+)
+def create_form(data: FormCreateModel, db: Session = Depends(get_db), user: AuthUser = Depends(get_admin_user)) -> None:
+    try:
+        # Variables
+        form_name = str(data.name).strip().lower()
+
+        # Validate form
+        form_data = db.execute(select(Form).where(Form.name == form_name)).scalar_one_or_none()
+        if form_data:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Form with name '{form_name}' already exists.")
+
+        # Create a record
+        form = Form(
+            created_by=user.id,
+            updated_by=user.id,
+            name=form_name,
+            content=data.content,
+        )
+        db.add(form)
+        db.commit()
+
+        # Return the created form
+        return {}
+    except Exception as e:
+        logger.exception(e)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"There is a problem with creating new form.")
+
+
+@router.put(
+    "/edit/{form_id}",
+    status_code=status.HTTP_200_OK,
+    name="Edit Form",
+    dependencies=[Depends(get_admin_user)],
+    response_model=FormResponseModel,
+)
+def edit_form(form_id: int, data: FormCreateModel, db: Session = Depends(get_db), user: AuthUser = Depends(get_admin_user)) -> None:
+    try:
+        # Fetch the order to ensure it exists
+        form = db.execute(select(Form).where(Form.id == form_id)).scalar_one_or_none()
+        if not form:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Form not found")
+        # Edit record
+        form.updated_by = user.id
+        form.updated_at = func.now()
+        form.content = data.content
+        db.commit()
+
+        # Return all companies from the database
+        return {}
+    except Exception as e:
+        logger.exception(e)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"There is a problem with guest registration.")
---
+++ src/v1/forms/schemas.py
@@ -0,0 +1,33 @@
+from datetime import datetime, date
+
+from pydantic import field_validator, BaseModel, EmailStr, Field
+
+from src.database.schemas import UserModel
+
+
+class FormModel(BaseModel):
+    id: int
+    updated_at: datetime
+    updated_by: int
+    name: str
+    content: str
+    updater: UserModel
+
+    # Pydantic configuration for datetime serialization
+    class Config:
+        from_attributes = True  # Allows ORM conversion
+        # json_encoders = {datetime: lambda v: v.date().isoformat()}  # Customize the format
+
+
+class FormsResponseModel(BaseModel):
+    name: str
+    content: str
+
+
+class FormCreateModel(BaseModel):
+    name: str
+    content: str
+
+
+class FormResponseModel(BaseModel):
+    success: bool = True
